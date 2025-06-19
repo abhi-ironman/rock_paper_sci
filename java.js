a0e4b725d46ee990240f89d03b8bdc25da1956e7
abhi-ironman <bharataabhishekbtech24@rvu.edu.in>
2025-06-19 20:57:24 +0530

    editted html and js files

--- java.js
+++ java.js
@@ -1,79 +1,105 @@
+const buttons = document.querySelectorAll("button");
+let humanScore = parseInt(document.querySelector("#humanScore").textContent);
+let computerScore = parseInt(document.querySelector("#computerScore").textContent);
+let count = 1;
+let flag = true;
+buttons.forEach(function(button){
+    button.addEventListener("click",function(e){
+        if(flag){
+            const humanChoice = e.target.textContent;
+            const computerChoice = getComputerChoice();
+            const choices = document.querySelector(".choices");
+            const round = document.createElement("p");
+            const p1 = document.createElement("p");
+            const p2 = document.createElement("p");
+            const p3 = document.createElement("p");
+            const play = playRround(computerChoice,humanChoice);
+            round.innerText = `Round :${count}`;
+            count++;
+            p1.innerText = humanChoice;
+            p2.innerText = computerChoice;
+            p3.innerText = play;
+            document.querySelector("#humanScore").textContent = humanScore;
+            document.querySelector("#computerScore").textContent = computerScore;
+            choices.appendChild(round);
+            choices.appendChild(p1);
+            choices.appendChild(p2);
+            choices.appendChild(p3);
+            if( humanScore == 5){
+                const p4 = document.createElement("p");
+                p4.innerText = "HUMANS WINN" ;
+                choices.appendChild(p4);
+                const btn = document.createElement("button");
+                btn.innerText = "RESET";
+                choices.appendChild(btn);
+                btn.addEventListener("click",function(e){
+                    choices.innerHTML="";
+                });
+                flag = false
+            }
+            else if( computerScore == 5){
+                const p4 = document.createElement("p");
+                p4.innerText = "COMPUTER WINN" ;
+                choices.appendChild(p4);
+                   const btn = document.createElement("button");
+                btn.innerText = "RESET";
+                choices.appendChild(btn);
+                btn.addEventListener("click",function(e){
+                    choices.innerHTML="";
+                });
+                flag = 0;
+            }
+        }
+    })
+});
 
-const btn1 = document.querySelector("#btn1");
-const btn2 = document.querySelector("#btn2");
-const btn3 = document.querySelector("#btn3");
-const human = document.querySelector("#human");
-const computer = document.querySelector("#computer");
-const winner = document.querySelector("#winner");
-let humanScore = 0; 
-let computerScore = 0;
+
 function getComputerChoice(){
-    let arr=["rock","paper","scissors"];
-    return arr[Math.floor(Math.random()*(2-0+1)+0)];
+    const choices = ["rock","paper","scissors"];
+    const index = Math.floor(Math.random()*3);
+    return choices[index];
 }
 
 function playRround(ComputerChoice,HumanChoice){
     if(ComputerChoice == "rock"){
         if(HumanChoice == "paper"){
-            humanScore+=1;
-           
-            console.log("You Win! Paper beats Rock.")
+            humanScore++;
+            return ("You Win! Paper beats Rock.")
         }
         else if(HumanChoice == "scissors"){
-            computerScore+=1;
-            console.log("You lose !! Rock beats Scissors.")
+            computerScore++;
+            return ("You lose !! Rock beats Scissors.")
         }
         else{
-            console.log("Its a DRAW!!!.")
+            return ("Its a DRAW!!!.")
         }
     }
 
     else if(ComputerChoice == "paper"){
         if(HumanChoice == "scissors"){
-            humanScore+=1;
-            console.log("You Win! scissors beats paper.")
+            humanScore++;
+                return ("You Win! scissors beats paper.")
         }
         else if(HumanChoice == "rock"){
-            computerScore+=1;
-            console.log("You lose !! Paper beats Rock.")
+            computerScore++;
+            return ("You lose !! Paper beats Rock.")
         }
         else{
-            console.log("Its a DRAW!!!.")
+            return ("Its a DRAW!!!.")
         }
     }
 
     else{
         if(HumanChoice == "rock"){
-            humanScore+=1;
-            console.log("You Win! Rock beats Scissors.")
+            humanScore++;
+            return ("You Win! Rock beats Scissors.")
         }
         else if(HumanChoice == "paper"){
-            computerScore+=1;
-            console.log("You lose !! Scissors beats Paper.")
+            computerScore++;
+            return ("You lose !! Scissors beats Paper.")
         }
         else{
-            console.log("Its a DRAW!!!.")
+            return ("Its a DRAW!!!.")
         }
     }
 }
-function playGame(HumanChoice,round){
-    let ComputerChoice=getComputerChoice();
-    console.log(`Round ${round}:`);
-    playRround(ComputerChoice,HumanChoice);
-    human.textContent=humanScore;
-    computer.textContent=computerScore;
-    if(humanScore == 5){
-        console.log("HUMANS WINNN");
-        winner.textContent="HUMANS WINNN"
-
-    }
-    else if(computerScore ==5){
-        console.log("COMPUTERS WINNN");
-        winner.textContent="COMPUTER WINSS"
-    }
-}
-let round = 1
-btn1.addEventListener("click",()=> playGame(btn1.textContent,round++));
-btn2.addEventListener("click",()=> playGame(btn2.textContent,round++) );
-btn3.addEventListener("click",()=> playGame(btn3.textContent,round++));
-
